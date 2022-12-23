5c2c24a1d6934695b16bde20c55a22f2427381aa
Jsgithubchannel <jsschannel99@gmail.com>
2022-12-23 14:56:33 +0900

    Feat: add icons in advantages

--- src/views/Service/components/Advantages/Advantages.tsx
+++ src/views/Service/components/Advantages/Advantages.tsx
@@ -1,11 +1,9 @@
-/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 const mock = [
@@ -14,21 +12,12 @@ const mock = [
     subtitle:
       'Indicates the possibility of data modeling through training to learn causality (cause and effect) from data',
     icon: (
-      <svg
-        height={24}
-        width={24}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
-        />
-      </svg>
+      <img
+        src="icons/versatile.png"
+        alt="versatile_icon"
+        height={48}
+        width={48}
+      />
     ),
   },
   {
@@ -36,21 +25,12 @@ const mock = [
     subtitle:
       'Indicates the importance/relevance of each feature for the prediction of labels',
     icon: (
-      <svg
-        height={24}
-        width={24}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-        />
-      </svg>
+      <img
+        src="icons/accuracy.png"
+        alt="accuracy_icon"
+        height={48}
+        width={48}
+      />
     ),
   },
   {
@@ -58,21 +38,12 @@ const mock = [
     subtitle:
       'May create accurate hypothetical situations by altering features in data',
     icon: (
-      <svg
-        height={24}
-        width={24}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z"
-        />
-      </svg>
+      <img
+        src="icons/insightful.png"
+        alt="insightful_icon"
+        height={48}
+        width={48}
+      />
     ),
   },
   {
@@ -80,21 +51,12 @@ const mock = [
     subtitle:
       'Clearly indicates to the user how it came to its results or conclusion',
     icon: (
-      <svg
-        height={24}
-        width={24}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M13 10V3L4 14h7v7l9-11h-7z"
-        />
-      </svg>
+      <img
+        src="icons/explainable.png"
+        alt="explainable_icon"
+        height={64}
+        width={64}
+      />
     ),
   },
   {
@@ -102,21 +64,12 @@ const mock = [
     subtitle:
       'As CCN’s are explainable, the relationship between humans and machines don’t have to be one-way or transactional any longer',
     icon: (
-      <svg
-        height={24}
-        width={24}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-        />
-      </svg>
+      <img
+        src="icons/thrustworthy.png"
+        alt="thrustworthy_icon"
+        height={48}
+        width={48}
+      />
     ),
   },
   {
@@ -124,21 +77,12 @@ const mock = [
     subtitle:
       'Reconstructs data through causal inference when some features of observational data are omitted',
     icon: (
-      <svg
-        height={24}
-        width={24}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-        />
-      </svg>
+      <img
+        src="icons/reconstructable.png"
+        alt="reconstructable_icon"
+        height={48}
+        width={48}
+      />
     ),
   },
 ];
